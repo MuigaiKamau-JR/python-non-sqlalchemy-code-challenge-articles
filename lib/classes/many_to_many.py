d0318fcf00638dc74675e5e526cef7101523ee99
Kamau <kamauwamuigaijr@gmail.com>
2024-06-14 19:16:29 +0300

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,123 @@
 class Article:
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
     def articles(self):
-        pass
-
-    def magazines(self):
-        pass
+        return self._articles
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        return list(set([article.magazine.category for article in self._articles]))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Category must be a string")
+        if not value:
+            raise ValueError("Category cannot be empty")
+        self._category = value
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
-    def contributors(self):
-        pass
 
-    def article_titles(self):
-        pass
 
-    def contributing_authors(self):
-        pass
+
+
+
+
+
+
+
+
+# class Article:
+#     def __init__(self, author, magazine, title):
+#         self.author = author
+#         self.magazine = magazine
+#         self.title = title
+        
+# class Author:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def articles(self):
+#         pass
+
+#     def magazines(self):
+#         pass
+
+#     def add_article(self, magazine, title):
+#         pass
+
+#     def topic_areas(self):
+#         pass
+
+# class Magazine:
+#     def __init__(self, name, category):
+#         self.name = name
+#         self.category = category
+
+#     def articles(self):
+#         pass
+
+#     def contributors(self):
+#         pass
+
+#     def article_titles(self):
+#         pass
+
+#     def contributing_authors(self):
+#         pass
--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import ipdb;
+import ipdb
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
@@ -8,6 +8,13 @@ from classes.many_to_many import Magazine
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    # Sample data for debugging
+    author_1 = Author("Carry Bradshaw")
+    magazine_1 = Magazine("Vogue", "Fashion")
+    article_1 = Article(author_1, magazine_1, "How to wear a tutu with style")
+    
+    print(magazine_1.articles())
+    print(author_1.articles())
 
-    # don't remove this line, it's for debugging!
+    # don't remove this line, ni ya debugging!
     ipdb.set_trace()
